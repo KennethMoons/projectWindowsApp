712a0146519bc125e717ccb4b793a58ad184ad2f
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2016-12-13 20:55:49 +0100

    LoginController now returns logged in user

--- WebApiOpendeurdag2/WebApiOpendeurdag2/Controllers/LoginController.cs
+++ WebApiOpendeurdag2/WebApiOpendeurdag2/Controllers/LoginController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiOpendeurdag2.Models;
+using WebApiOpendeurdag2.Security;
 
 namespace WebApiOpendeurdag2.Controllers
 {
-    [Authorize]
     public class LoginController : ApiController
     {
-        public bool GetLogin()
+        [ResponseType(typeof(Gebruiker))]
+        public IHttpActionResult GetLogin()
         {
-            return true;
+            // Check if user is set
+            if (!(HttpContext.Current.User is APIPrincipal))
+            {
+                return NotFound();
+            }
+
+            // Return current user
+            APIPrincipal principal = (APIPrincipal)HttpContext.Current.User;
+
+            return Ok(principal.User);
         }
     }
 }
