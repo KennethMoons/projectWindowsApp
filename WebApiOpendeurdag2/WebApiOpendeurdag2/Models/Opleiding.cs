7153f258be9fb5a353a6671af81f06185b1759d0
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2016-12-19 08:46:44 +0100

    Added equals & hashcode to Campus & Opleiding

--- WebApiOpendeurdag2/WebApiOpendeurdag2/Models/Opleiding.cs
+++ WebApiOpendeurdag2/WebApiOpendeurdag2/Models/Opleiding.cs
@@ -12,5 +12,21 @@ namespace WebApiOpendeurdag2.Models
         [Required]
         public String Naam { get; set; }
         public String Type { get; set; }
+        public override bool Equals(object obj)
+        {
+            Opleiding item = obj as Opleiding;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.OpleidingId.Equals(item.OpleidingId);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.OpleidingId.GetHashCode();
+        }
     }
 }
