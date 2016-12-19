3985e4a56b8b424bcf6d06ac1a7c66cd06682d41
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2016-12-19 23:10:01 +0100

    Additional data added

--- ProjectOpendeurdag/ProjectOpendeurdag/GebruikersBeheerMain.xaml.cs
+++ ProjectOpendeurdag/ProjectOpendeurdag/GebruikersBeheerMain.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using ProjectOpendeurdag.Models;
 using ProjectOpendeurdag.Helpers;
+using Prism.Mvvm;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,29 +27,81 @@ namespace ProjectOpendeurdag
     /// </summary>
     public sealed partial class GebruikersBeheerMain : Page
     {
+        public GebruikersModel Gebruikers { get; set; }
+
         public GebruikersBeheerMain()
         {
             this.InitializeComponent();
-            getAantalGebruikers();
+            this.Loaded += GebruikersBeheerMain_Loaded;
+        }
+
+        private async void GebruikersBeheerMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            Gebruikers = DataContext as GebruikersModel;
+
+            var gebruikers = await Api.GetAsync<List<Gebruiker>>();
+
+            Gebruikers.Aantal = gebruikers.Count;
+
+            var campussen = gebruikers
+                .SelectMany(g => g.VoorkeurCampussen)
+                .GroupBy(c => c)
+                .OrderBy(g => g.Count())
+                .Reverse()
+                .Select(g => g.Key);
+
+            Gebruikers.PopulairsteCampus = campussen.FirstOrDefault();
+            Gebruikers.MinstPopulaireCampus = campussen.LastOrDefault();
+
+            var opleidingen = gebruikers
+                .SelectMany(g => g.VoorkeurOpleidingen)
+                .GroupBy(c => c)
+                .OrderBy(g => g.Count())
+                .Reverse()
+                .Select(g => g.Key);
+
+            Gebruikers.PopulairsteOpleiding = opleidingen.FirstOrDefault();
+            Gebruikers.MinstPopulaireOpleiding = opleidingen.LastOrDefault();
         }
 
         private void BekijkLijst(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(GebruikersBeheerDetail));
         }
+    }
+    public class GebruikersModel : BindableBase
+    {
+        private int aantal;
+        private Campus populairsteCampus;
+        private Campus minstPopulaireCampus;
+        private Opleiding populairsteOpleiding;
+        private Opleiding minstPopulaireOpleiding;
 
-        public async void getAantalGebruikers()
+        public int Aantal
         {
-            List<Gebruiker> gebruikers = await Api.GetAsync<List<Gebruiker>>();
-            int aantal = gebruikers.Count - 1;
-            aantalStudenten.Text = aantal.ToString();
+            get { return aantal; }
+            set { SetProperty(ref aantal, value); }
         }
-        public async void getCampusVoorkeurInfo()
+        public Opleiding PopulairsteOpleiding
         {
+            get { return populairsteOpleiding; }
+            set { SetProperty(ref populairsteOpleiding, value); }
         }
-        public async void getOpleidingvoorkeurInfo()
+        public Opleiding MinstPopulaireOpleiding
         {
+            get { return minstPopulaireOpleiding; }
+            set { SetProperty(ref minstPopulaireOpleiding, value); }
+        }
+        public Campus PopulairsteCampus
+        {
+            get { return populairsteCampus; }
+            set { SetProperty(ref populairsteCampus, value); }
+        }
+        public Campus MinstPopulaireCampus
+        {
+            get { return minstPopulaireCampus; }
+            set { SetProperty(ref minstPopulaireCampus, value); }
+        }
 
-        }
     }
 }
